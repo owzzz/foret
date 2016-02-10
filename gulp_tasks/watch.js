e2d3dc68f68fe4e12ce9b5e32792c0d3d09db892
Owain Llewellyn <Owain.Llewellyn@akqa.com>
2016-02-10 18:14:13 -0500

    Added files

--- gulp_tasks/watch.js
+++ gulp_tasks/watch.js
@@ -1,17 +1,3 @@
 /*eslint-disable strict */
 'use strict';
 
-const gulp = require('gulp');
-const gulpUtil = require('gulp-util');
-const CONSTS = require('./constants');
-
-const watchSass = gulp.watch(CONSTS.CSS_SRC + '**/*.{scss,sass}', ['sass']);
-const watchScripts = gulp.watch(CONSTS.JS_SRC + '**/*.js', ['compress']);
-const watchTemplates = gulp.watch(CONSTS.SRC + '**/*.html', ['copyTemplates']);
-const watchImages = gulp.watch(CONSTS.SRC + '**/*.{gif,jpg,png}', ['imagemin']);
-
-[watchImages, watchSass, watchScripts, watchTemplates].forEach((w) => {
-	w.on('change', (e) => {
-		gulpUtil.log(e.path, e.type);
-	});
-});
